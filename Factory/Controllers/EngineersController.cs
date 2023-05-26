2d9420972a3fdb1af1ee15ff357645b71e55f87a
MarcusKyung <Kyungmj@gmail.com>
2023-05-26 09:25:44 -0700

    add engineers and machines index()

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+namespace Factory.Controllers
+{
+  public class EngineerController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineerController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.OrderBy(engineer => engineer.EngineerName).ToList();
+      return View(model);
+    }
+  }
+}
